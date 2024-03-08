8792232d925e5b0fbf8f4ea27f9ea15b6a0dce09
manu <zxthz@126.com>
2024-03-08 17:08:33 +0800

    debug

--- cores/particles_detector.py
+++ cores/particles_detector.py
@@ -11,7 +11,7 @@ class ParticlesDetector:
     def infer(self):
         if self.db.seq_len < ALARM_NAIVE_SEQ_LEN:
             return
-        seq_pm025 = np.array(self.db.db['pm025'][-ALARM_NAIVE_SEQ_LEN]).astype(float)
-        seq_voc = np.array(self.db.db['voc'][-ALARM_NAIVE_SEQ_LEN]).astype(float)
+        seq_pm025 = np.array(self.db.db['pm025'][-1]).astype(float)
+        seq_voc = np.array(self.db.db['voc'][-1]).astype(float)
         if np.average(seq_pm025) > ALARM_NAIVE_THRESHOLD_PM025 and np.average(seq_voc) > ALARM_NAIVE_THRESHOLD_VOC:
             self.db.db['alarm'][-1] = ALARM_INDICATE_VAL
