6b385b0cbf4d5911e44215d4dc99f7e16fedbadf
manu <zxthz@126.com>
2024-03-15 16:24:14 +0800

    add more types of parser

--- demos/demo_parser.py
+++ demos/demo_parser.py
@@ -1,13 +1,12 @@
 import argparse
-import logging
 
-from parsers.parser import ParserV2
+from parsers.parser import *
 from utils.utils import set_logging
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--addr', default='/media/manu/data/docs/particles/热解粒子实验数据/涿鹿交付')
+    parser.add_argument('--addr', default='/media/manu/data/docs/particles/热解粒子实验数据/涿鹿交付/烟感_正庚烷火')
     parser.add_argument('--db_type', default='DataTextV3')
     parser.add_argument('--suffix', default='txt')
     parser.add_argument('--dir_plot_save', default='/home/manu/tmp/demo_parser_save')
@@ -16,7 +15,8 @@ def parse_args():
 
 def run(args):
     logging.info(args)
-    parser = ParserV2(db_type=args.db_type, suffix=args.suffix, dir_plot_save=args.dir_plot_save, dir_in=args.addr)
+    parser = ParserV1(db_type=args.db_type, suffix=args.suffix,
+                      dir_plot_save=args.dir_plot_save, addr_in=args.addr)
     parser.parse()
 
 
--- parsers/parser.py
+++ parsers/parser.py
@@ -9,8 +9,48 @@ class ParserBase:
         self.suffix = suffix
         self.dir_plot_save = dir_plot_save
 
+        make_dirs(self.dir_plot_save, reset=True)
+
     def parse(self):
         raise NotImplementedError
+
+
+class ParserV0(ParserBase):
+    """
+    format: file_name.suffix
+    """
+
+    def __init__(self, db_type, suffix, dir_plot_save, addr_in):
+        super().__init__(db_type, suffix, dir_plot_save)
+        self.path_in = addr_in
+
+    def parse(self):
+        logging.info(self.path_in)
+        db_obj = eval(self.db_type)(self.path_in)
+        db_obj.update()
+        path_save = os.path.join(self.dir_plot_save, os.path.basename(self.path_in))
+        path_save = path_save.replace(self.suffix, 'png')
+        db_obj.plot(pause_time_s=0.001, path_save=path_save)
+
+
+class ParserV1(ParserBase):
+    """
+    format: dir_in/file_name.suffix
+    """
+
+    def __init__(self, db_type, suffix, dir_plot_save, addr_in):
+        super().__init__(db_type, suffix, dir_plot_save)
+        self.dir_in = addr_in
+
+    def parse(self):
+        paths_in = glob.glob(os.path.join(self.dir_in, '*'))
+        for path_in in paths_in:
+            logging.info(path_in)
+            db_obj = eval(self.db_type)(path_in)
+            db_obj.update()
+            path_save = os.path.join(self.dir_plot_save, os.path.basename(path_in))
+            path_save = path_save.replace(self.suffix, 'png')
+            db_obj.plot(pause_time_s=0.001, path_save=path_save)
 
 
 class ParserV2(ParserBase):
@@ -18,10 +58,9 @@ class ParserV2(ParserBase):
     format: dir_in/case_name/file_name.suffix
     """
 
-    def __init__(self, db_type, suffix, dir_plot_save, dir_in):
+    def __init__(self, db_type, suffix, dir_plot_save, addr_in):
         super().__init__(db_type, suffix, dir_plot_save)
-        self.dir_in = dir_in
-        make_dirs(self.dir_plot_save, reset=True)
+        self.dir_in = addr_in
 
     def parse(self):
         dirs_case = glob.glob(os.path.join(self.dir_in, '*'))
